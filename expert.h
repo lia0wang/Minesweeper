76be9ded69bf363bb3cfd7ed06ec942a02b7eb05
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-10-23 01:07:21 +1100

    Initilize files

---
+++ expert.h
@@ -0,0 +1,10 @@
+/*
+ * COMP1521 21T2 -- Assignment 1 -- Minesweeper Constants!
+ *
+ * Version 1.0 (08-10-21): Team COMP1521 <cs1521@cse.unsw.edu.au>
+ */
+
+#define MAX_BOMBS       90
+#define N_ROWS          16
+#define N_COLS          30
+#define N_CELLS         N_ROWS * N_COLS
